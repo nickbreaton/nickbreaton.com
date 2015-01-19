e7a54ecf0013d924c3bd74edde883b923e12b4a2
Nick Breaton <nick@breaton.com>
2015-01-18 22:01:18 -0500

    Added intervals to clock.

--- client/build/site.js
+++ client/build/site.js
@@ -26,3 +26,30 @@ app.run(['$route', "$rootScope", function($route, $rootScope)  {
     return "/static/templates/" + name + ".html";
   }
 }]);
+
+app.controller("ClockController", ["$scope", function ($scope) {
+  createIntervals($scope);
+
+
+  function createIntervals ($scope) {
+    $scope.intervals = [];
+
+    for (var i = 1; i <= 60; i++) {
+      var size = "normal";
+
+      if (i % 5 === 0) {
+        size = "medium";
+      }
+
+      if (i % 15 === 0) {
+        size = "large";
+      }
+
+      $scope.intervals.push({
+        id : i,
+        type : size
+      });
+    }
+  }
+
+}]);
