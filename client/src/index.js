80c263e3274327bc1673172219380af0101a6016
Nick Breaton <nick@breaton.com>
2015-01-18 22:10:15 -0500

    Use strict.

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // create app
 var app = angular.module("my-site", ["ngRoute"]);
 
@@ -21,7 +23,7 @@ app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $lo
 // make sure nested views and includes loads
 app.run(['$route', "$rootScope", function($route, $rootScope)  {
   $route.reload();
-  
+
   $rootScope.template = function (name) {
     return "/static/templates/" + name + ".html";
   }
