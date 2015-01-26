f45a87ff025ecb45c6991a1413bd6cff3fc73a6c
Nick Breaton <nick@breaton.com>
2015-01-26 11:32:40 -0500

    Map pin popup on pin scroll.

--- client/build/site.js
+++ client/build/site.js
@@ -168,60 +168,79 @@ app.controller("HeaderController", ["$scope", "$location", "$rootScope", "SiteIn
   }
 }]);
 
-app.controller("MapController", ["$scope", "$rootScope", "MapStyle", function ($scope, $rootScope, MapStyle) {
-  var canvas;
-  var parent;
+app.controller("MapController", ["$scope", "$rootScope", "$window", "MapStyle", function ($scope, $rootScope, $window, MapStyle) {
+  map();
+  pin();
 
-  setInterval(function () {
-    $scope.pinActive = true;
+  function pin () {
     setTimeout(function () {
-      $scope.pinActive = false;
-    }, 1500);
-  }, 3000);
+      watch();
+      angular.element($window).bind('scroll', watch);
+    }, 1000);
 
+    function watch() {
+      var map = document.getElementById("map-frame").getBoundingClientRect();
+      var pin = document.getElementById("map-pin").getBoundingClientRect();
 
-  $rootScope.mapWidth = undefined;
-
-  if (google.maps.event) {
-    google.maps.event.addDomListener(window, 'resize', function () {
-      canvas = document.getElementById('map-canvas');
-      parent = canvas.parentNode;
-
-      if ($rootScope.mapWidth != parent.getBoundingClientRect().width) {
-        create();
-        $rootScope.mapWidth = parent.getBoundingClientRect().width;
-      }
-    });
-  }
-
-  // make sure event runs every time controller is activated
-  window.dispatchEvent(new Event('resize'));
-
-  function create () {
-    if (google) {
-      var style = MapStyle("#7c9b8b");
-
-      var letters = "ABCDEFGHIJKLMNOPQRSTUV";
-      var randId = "";
-
-      for (var i = 0; i < 10; i++) {
-        randId += letters[Math.floor(Math.random() * letters.length)];
+      if (map.bottom * .85 < window.innerHeight) {
+        $scope.pinActive = true;
+        $scope.$apply();
       }
 
-      var mapOptions = {
-        zoom: 13,
-        center: new google.maps.LatLng(35.7303443,-78.8500691),
-        mapTypeId : randId
-      };
-
-      var map = new google.maps.Map(canvas, mapOptions);
-
-      var customMapType = new google.maps.StyledMapType(style, {name: 'Custom Style'});
-
-      map.mapTypes.set(randId, customMapType);
+      if (pin.top > window.innerHeight) {
+        $scope.pinActive = false;
+        $scope.$apply();
+      }
     }
   }
 
+
+  function map() {
+    var canvas;
+    var parent;
+
+    $rootScope.mapWidth = undefined;
+
+    if (google.maps.event) {
+      google.maps.event.addDomListener(window, 'resize', function () {
+        canvas = document.getElementById('map-canvas');
+        parent = canvas.parentNode;
+
+        if ($rootScope.mapWidth != parent.getBoundingClientRect().width) {
+          create();
+          $rootScope.mapWidth = parent.getBoundingClientRect().width;
+        }
+      });
+    }
+
+    // make sure event runs every time controller is activated
+    window.dispatchEvent(new Event('resize'));
+
+    function create () {
+      if (google) {
+        var style = MapStyle("#7c9b8b");
+
+        var letters = "ABCDEFGHIJKLMNOPQRSTUV";
+        var randId = "";
+
+        for (var i = 0; i < 10; i++) {
+          randId += letters[Math.floor(Math.random() * letters.length)];
+        }
+
+        var mapOptions = {
+          zoom: 13,
+          center: new google.maps.LatLng(35.7303443,-78.8500691),
+          mapTypeId : randId
+        };
+
+        var map = new google.maps.Map(canvas, mapOptions);
+
+        var customMapType = new google.maps.StyledMapType(style, {name: 'Custom Style'});
+
+        map.mapTypes.set(randId, customMapType);
+      }
+    }
+  }
 }]);
 
 app.constant("MapStyle", function (color) {
