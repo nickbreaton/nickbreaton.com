fcf8e97a45f604f8913950f998d1844bd0e6ee12
Nick Breaton <nick@breaton.com>
2015-01-27 13:31:44 -0500

    Modulated styling for about page.

--- client/src/providers/SiteInfo.js
+++ client/src/providers/SiteInfo.js
@@ -4,7 +4,7 @@ app.factory("SiteInfo", function () {
   this.name = this.firstName + " " + this.lastName;
   this.nameIntro = "Hi, I'm";
   this.moto = "This is how I spend my time."
-  this.jobTitle = "Front End Developer";
+  this.jobTitle = "Web Developer";
 
   return this;
 });
