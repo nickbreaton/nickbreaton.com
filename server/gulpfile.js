b05cdcd2666637411ebae2d8fed89edd7a45c0cb
Nick Breaton <nick@breaton.com>
2015-01-29 11:47:12 -0500

    Index resource minifier working correctly.

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -33,7 +33,7 @@ gulp.task("index", function () {
   return gulp.src(path.join(paths.src, "index.html"))
     .pipe(gulp.dest(paths.index))
     .pipe(plugin.frep([{
-      pattern : /.js/g,
+      pattern : /\.js/g,
       replacement : ".min.js"
     }]))
     .pipe(gulp.dest(path.join(paths.index, "min")));
