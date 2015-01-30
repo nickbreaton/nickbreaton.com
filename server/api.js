6daec186ca415a9ff1043e215ef273a9a0159154
Nick Breaton <nick@breaton.com>
2015-01-29 22:01:15 -0500

    Added basic API. Added mongodb connection.

---
+++ server/api.js
@@ -0,0 +1,48 @@
+var db, ready;
+
+module.exports.connect = function () {
+  var fs = require("fs");
+  var mongoose = require("mongoose");
+  var path = require("path");
+  var paths = require("./paths");
+
+  var username = getCredential("username");
+  var password = getCredential("password");
+  var hostname = getCredential("hostname");
+
+  ready = false;
+
+  function getCredential (credential) {
+    return (fs.readFileSync(path.join(paths.server, "credentials", credential), "utf8")).replace(/\n$/, '');
+  }
+
+  var uri = "mongodb://" + username + ":" + password + "@" + hostname + "/" + "testdb";
+  mongoose.connect(uri, { auth: { authdb:"admin" } });
+
+  mongoose.connection.once('connected', function() {
+    db = mongoose.connection.db;
+    ready = true;
+  });
+}
+
+module.exports.route = function (app) {
+  // ensure API service is ready
+  app.use("/api/*", function (req, res, next) {
+    ready ? next() : res.writeHead(202);
+  });
+
+  // sample return
+  app.get("/api/my-name/", function (req, res, next) {
+    db.collection("testcoll", function (err, collection) {
+      collection.find().toArray(function (err, docs) {
+        res.end(JSON.stringify(docs[0].name));
+      });
+    });
+  });
+
+  // catch all api
+  app.get("/api/*", function(req, res, next) {
+    res.writeHead(404);
+    res.end("API not found.");
+  });
+}
--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 var express = require("express");
 
 var routes = require("./routes");
+var api = require("./api");
 
 var app = express();
+
+api.connect();
 
 app.set("port", 3000);
 app.set("env", process.env.NODE_ENV);
 app.set("dev", app.get("env") != "production");
+app.set("api", api);
 
 app.use(function (req, res, next) {
+  api.route(app);
   routes(app);
   next();
 });
