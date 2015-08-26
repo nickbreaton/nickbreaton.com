c3751e94fe6dfe505b304c97c0a9aa8af11f0191
Nick Breaton <nick@breaton.com>
2015-08-25 23:11:52 -0400

    Finish restructure with completion of build config.

--- config/build.js
+++ config/build.js
@@ -1,6 +1,14 @@
 import webpack from 'webpack'
 import config from './webpack'
 
+// ready files for production
+config.plugins.push(
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.UglifyJsPlugin()
+);
+
+// run webpack configuration to build production ready application
 webpack(config, (err) => {
   err ? console.log(err) : console.log('Done.');
 });
