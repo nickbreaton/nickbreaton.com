0966831f346d7a92e2db8b05c50f767ae0ecab22
nickbreaton <nick@breaton.com>
2018-08-19 18:54:49 -0400

    More lighthouse enhancements

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,35 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components'
-  ],
+    {
+      resolve: 'gatsby-plugin-react-helmet'
+    },
+    {
+      resolve: 'gatsby-plugin-styled-components'
+    },
+    {
+      resolve: 'gatsby-plugin-netlify'
+    },
+    {
+      resolve: 'gatsby-plugin-offline'
+    },
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Nick Breaton',
+        short_name: 'Nick',
+        start_url: '/',
+        background_color: '#FFF',
+        theme_color: '#FFF',
+        display: 'minimal-ui',
+        icon: 'src/assets/favicon.png',
+      },
+    },
+  ]
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-
-// import { StaticQuery, graphql } from "gatsby"
-
-// const TitleQuery = ({ children }) => (
-//   <StaticQuery
-//     query={graphql`
-//       query SiteTitleQuery {
-//         site {
-//           siteMetadata {
-//             title
-//           }
-//         }
-//       }
-//     `}
-//     render={children}
-//   />
-// )
+import favicon from '../assets/favicon.png';
 
 const Layout = ({ children }) => (
   <>
     <Helmet>
+      <html lang='en-us' />
       <title>Nick Breaton</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta name='description' content='Software Engineer' />
-      <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Open+Sans' />
-      <link rel='icon' type='image/png' href='favicon.png' />
+      <link rel='icon' type='image/png' href={favicon} />
     </Helmet>
     {children}
   </>
