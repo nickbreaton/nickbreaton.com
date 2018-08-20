3a4b29b97da9a1ab4fae9ee19f612adefc5a3fbf
nickbreaton <nick@breaton.com>
2018-08-19 20:55:42 -0400

    Remove blog for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import me from '../assets/me.jpeg';
@@ -18,7 +17,7 @@ const ProfileImage = styled.img`
   border-radius: 100%;
 `;
 
-const ExternalLink = styled.a`
+const Link = styled.a`
   color: #16a085;
   text-decoration: none;
   &::after {
@@ -30,22 +29,16 @@ const ExternalLink = styled.a`
   }
 `;
 
-const InteralLink = ExternalLink.withComponent(Link);
-
 const IndexPage = () => (
   <Layout>
     <Content>
       <h1>Hi, I'm Nick Breaton</h1>
-      <ProfileImage
-        alt='Nick Breaton'
-        src={me}
-      />
+      <ProfileImage alt='Headshot of Nick Breaton' src={me} />
       <h2>Software Engineer</h2>
       <div>
-        {/* <InteralLink to='blog/'>Blog</InteralLink> */}
-        <ExternalLink href='https://github.com/nickbreaton'>GitHub</ExternalLink>
-        <ExternalLink href='https://www.linkedin.com/in/nickbreaton'>LinkedIn</ExternalLink>
-        <ExternalLink href='mailto:nick@breaton.com'>Email</ExternalLink>
+        <Link href='https://github.com/nickbreaton'>GitHub</Link>
+        <Link href='https://www.linkedin.com/in/nickbreaton'>LinkedIn</Link>
+        <Link href='mailto:nick@breaton.com'>Email</Link>
       </div>
     </Content>
   </Layout>
