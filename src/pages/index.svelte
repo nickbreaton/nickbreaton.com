1f81b752406d9a7c8313c11f617106d6822638c8
Nick Breaton <nick@breaton.com>
2021-02-15 17:02:17 -0500

    Get _very_ basic blog going

---
+++ src/pages/index.svelte
@@ -0,0 +1,37 @@
+<script>
+    const links = [
+        ["GitHub", "https://github.com/nickbreaton"]
+    ]
+</script>
+
+<h1>Nick Breaton</h1>
+<p>Front End Engineer working <a href="https://creditkarma.com">Credit Karma</a></p>
+
+<h2>Links</h2>
+<ul>
+    <li>
+        <a href="https://github.com/nickbreaton">GitHub</a>
+    </li>
+    <li>
+        <a href="https://twitter.com/nickbreaton">Twitter</a>
+    </li>
+    <li>
+        <a href="https://www.linkedin.com/in/nickbreaton/">LinkedIn</a>
+    </li>
+    <li>
+        <a href="mailto:nick@breaton.com">Email</a>
+    </li>
+</ul>
+
+<h2>Articles</h2>
+<ul>
+    <li>
+        check back soon...
+    </li>
+    <!-- <li>
+        <a href="/posts/learning-svelte">Learning Svelte</a>
+    </li>
+    <li>
+        <a href="/posts/writing-a-rollup-plugin">Writing a Rollup Plugin</a>
+    </li> -->
+</ul>
---
+++ src/pages/posts/learning-svelte.svelte
@@ -0,0 +1,11 @@
+<h1>Learning Svelte</h1>
+
+<p>
+    <strong>February 15, 2020</strong>
+</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate cumque quas, ipsa non eaque saepe perferendis natus repellendus officia consectetur esse dolor quasi reprehenderit nam sit, veniam sapiente ipsam ratione?</p>
+
+<pre>
+    example code...
+</pre>
