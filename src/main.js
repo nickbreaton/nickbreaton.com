12db504e5e43b1c129cbd06e5749e5c8a4b1b69e
Nick Breaton <nick@breaton.com>
2021-02-15 17:17:00 -0500

    Remove service worker from existing site

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("noop")
+// remove existing service worker
+
+self.addEventListener("activate", function (e) {
+    self.registration
+        .unregister()
+        .then(function () {
+            return self.clients.matchAll()
+        })
+        .then(function (clients) {
+            clients.forEach((client) => client.navigate(client.url))
+        })
+})
